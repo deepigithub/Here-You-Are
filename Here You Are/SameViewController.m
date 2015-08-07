fa00b785a162003c6ce65ed11458cd57f35ce191
vmvinothkumar <vmvinothkumar@gmail.com>
2015-08-07 18:32:01 +0530

    commit same branch

--- Here You Are/SameViewController.m
+++ Here You Are/SameViewController.m
@@ -22,7 +22,7 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    //Change on same line branch
+    //Change on same branch
 }
 
 /*
